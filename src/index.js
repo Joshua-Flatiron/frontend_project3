35e2c6e6c073e1ce1301a5b3425c11d6afa7b6bc
myildiz17 <muhammetyildiz@gmail.com>
2020-11-10 10:34:39 -0600

    added some html

--- src/index.js
+++ src/index.js
@@ -17,14 +17,18 @@ const renderOneProject = (project) => {
     const projectDiv = ce('div')
     projectDiv.setAttribute('data-project-id', project.id)
     const title = ce('h1')
-    title.innerText = project.name
+    title.innerHTML += `${project.name} <button class= "edit-btn" id = "${project.id}">Edit</button> <button class= "delete-btn" id = "${project.id}">Delete</button> `
     const importance = ce('p')
     const time = ce('p')
     importance.innerText = `Importance: ${project.importance}`
     time.innerText = `Time needed: ${project.time}`
     
+    const taskTitle = ce('h2')
+    taskTitle.innerText = 'List of the tasks:'
+    const addNewTask = ce('button')
+    addNewTask.innerText = `<button class= "edit-btn" id = "${project.id}">Add new task</button>`
 
-    projectDiv.append(title, importance, time) 
+    projectDiv.append(title, importance, time, taskTitle, addNewTask) 
 
     project.tasks.forEach(task => {
             const newTask = addTask(task)
@@ -40,7 +44,7 @@ const addTask = (task) => {
     const taskDiv = ce('div')
             taskDiv.setAttribute('id', task.id)
             const tTitle = ce('h2')
-            tTitle.innerText = task.name
+            tTitle.innerHTML += `${task.name} <button class= "edit-btn" id = "${task.id}">Edit</button> <button class= "delete-btn" id = "${task.id}">Delete</button> `
             const tTime = ce('p')
             tTime.innerText = `Time needed: ${task.time}`
         
