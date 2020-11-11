b6ac999277c05039cce8a64c35b8e25794dac009
myildiz17 <muhammetyildiz@gmail.com>
2020-11-11 09:37:51 -0600

    forms location  and project delete complete

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
+let addProject = false;
 const projectURL = "http://localhost:3000/projects"
 const mainContainer = document.querySelector('#main-container')
+const projectFormContainer = document.querySelector(".projectContainer")
+const addBtn = document.querySelector("#new-project-btn")
+const addBtn2 = document.querySelector(".add-task-id")
+
 const ce = (element) => {
     return document.createElement(element)
 }
@@ -8,6 +13,7 @@ const fetchProject = () => {
     fetch(projectURL)
     .then(resp => resp.json())
     .then(data => renderProjects(data))
+    // debugger
 }
 
 
@@ -17,7 +23,31 @@ const renderOneProject = (project) => {
     const projectDiv = ce('div')
     projectDiv.setAttribute('data-project-id', project.id)
     const title = ce('h1')
-    title.innerHTML += `${project.name} <button class= "edit-btn" id = "${project.id}">Edit</button> <button class= "delete-btn" id = "${project.id}">Delete</button> `
+
+    delBtn = ce("button")
+    delBtn.className = "project-release"
+    delBtn.setAttribute("project-task-id", project.id)
+    delBtn.innerText = "Delete"
+
+    editBtn = ce("button")
+    editBtn.className = "project-edit"
+    editBtn.setAttribute("edit-project-id", project.id)
+    editBtn.innerText = "Edit"
+
+    delBtn.addEventListener("click", () => {
+        fetch(projectURL+"/"+project.id, {
+            method: "DELETE"
+        })
+        .then(resp => resp.json())
+        .then(console.log)
+        projectDiv.remove()
+    })
+
+    title.innerHTML += `${project.name} `
+    // <button class= "edit-btn" id = "${project.id}">Edit</button> <button class= "delete-btn" id = "${project.id}">Delete</button> `
+   
+    title.append("  ",delBtn, "         ",editBtn)
+    
     const importance = ce('p')
     const time = ce('p')
     importance.innerText = `Importance: ${project.importance}`
@@ -26,14 +56,52 @@ const renderOneProject = (project) => {
     const taskTitle = ce('h2')
     taskTitle.innerText = 'List of the tasks:'
     const addNewTask = ce('button')
-    addNewTask.innerText = `<button class= "edit-btn" id = "${project.id}">Add new task</button>`
+    addNewTask.innerText = "Add new task"
 
-    projectDiv.append(title, importance, time, taskTitle, addNewTask) 
+    const formAddTask = ce('div')
+        formAddTask.setAttribute('class', 'add-task-id' )
+            formAddTask.innerHTML = `        <form class="add-task-form">
+            
+            <input
+              type="text"
+              name="name"
+              value=""
+              placeholder="Task's name..."
+              class="input-text"
+            />
+            
+            <input
+              type="text"
+              name="name"
+              value=""
+              placeholder="Time needed..."
+              class="input-text"
+            />
+
+            <input
+              type="hidden"
+              name="image"
+              value="${project.id}"
+              placeholder="Project's name..."
+              class="input-text"
+            />
+            
+            <input
+              type="submit"
+              name="submit"
+              value="New Task"
+              class="submit"
+            />
+          </form>`
+
+    projectDiv.append(title, importance, time, taskTitle,formAddTask) 
+
 
     project.tasks.forEach(task => {
             const newTask = addTask(task)
             projectDiv.append(newTask)
         })
+        // debugger
 
     
     
@@ -41,14 +109,33 @@ const renderOneProject = (project) => {
 }
 
 const addTask = (task) => {
+
     const taskDiv = ce('div')
+
+            
+            delBtn = ce("button")
+            delBtn.className = "release"
+            delBtn.setAttribute("data-task-id", task.id)
+            delBtn.innerText = "X"
+
+            editBtn = ce("button")
+            editBtn.className = "task-edit"
+            editBtn.setAttribute("edit-task-id", task.id)
+            editBtn.innerText = "E"
+
+       
             taskDiv.setAttribute('id', task.id)
             const tTitle = ce('h2')
-            tTitle.innerHTML += `${task.name} <button class= "edit-btn" id = "${task.id}">Edit</button> <button class= "delete-btn" id = "${task.id}">Delete</button> `
+            tTitle.innerHTML += `${task.name}  `
+            //  <button class= "edit-btn" id = "${task.id}">Edit</button> <button class= "delete-btn" id = "${task.id}">Delete</button> `
+            // tTitle.append(delBtn)
             const tTime = ce('p')
             tTime.innerText = `Time needed: ${task.time}`
-        
+            tTitle.append("  ",delBtn," ",editBtn)
+
             taskDiv.append(tTitle, tTime)
+
+            
             return taskDiv
 }
 
@@ -61,3 +148,8 @@ const renderProjects = (projects) => {
 }
 
 fetchProject()
+
+addBtn.addEventListener("click", () => {
+    addProject = !addProject;
+    addProject ? projectFormContainer.style.display = "block" : projectFormContainer.style.display = "none";
+  });
